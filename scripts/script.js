3fababfe8ec623819fdf6be9a6ee308167308db5
thesalientone <stefan.jenkins@gmail.com>
2017-06-19 21:49:02 -0400

    Added some jquery

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+
+
+
+$(document).ready(function(){
+
+  var $pendown = $("#pendown");
+  var $clickcolor = $("#clickcolor");
+  var $selectcolor = $("#selectcolor");
+  var $selectcolor = $("#templatecolor");
+  var $selectcolor = $("#randomcolor");
+
+
+  function createDivs(number) {
+
+
+
+  }
+  
+})
